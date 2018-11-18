30e7e9cfb05b1d7123727efca66d076b51bbb546
Tsyren <nsu1team@gmail.com>
2018-11-18 18:43:06 +0100

    guessing-game-tutorial.html#processing-a-guess

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::io; // listed, without we would call `std::io::stdid()`.
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+
+    println!("Please enter your guess.");
+
+    let mut guess = String::new(); //  let - def variable, mu - makes it mutable, String is stdlib type that is growable, UTF-8 encoded bit of text. :: - accessor to assoc methods of `type`.
+
+    io::stdin().read_line(&mut guess)
+        .expect("Failed to read line"); // io::stdin() => instance of io::Stdin, returns `io::Result` => `Ok` or `Err`
+
+    println!("You guessed: {}", guess);
 }
